de33641fa3069fa724da12a77d70e0183cd17177
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-07 12:51:50 -0800

    webpack tutorial not working. keep getting cannot use import statement outside a moduel chrome error

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,22 @@ module.exports = {
   },
 
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
+    extensions: [ '.js', '.jsx' ]
+  },
 
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "es2015",
+            "react"
+          ]
+        }
+      },
+    ],
+  }
 };
