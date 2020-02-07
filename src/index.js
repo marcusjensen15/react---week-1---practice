bc8d73b93e636a7f9d4a71ef9f6236af5fef7506
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-07 10:28:32 -0800

    clock now updating time every second

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+function liveTime(){
+
 var greeting = React.createElement('h1', {}, 'Hello, World!');
 var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
 var app = React.createElement('div', {}, greeting, clock);
@@ -10,3 +12,7 @@ ReactDOM.render(
   app,
   document.getElementById('react-app-root')
 );
+
+}
+
+setInterval(liveTime, 1000);
