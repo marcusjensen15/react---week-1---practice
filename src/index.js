15c075286a47763ec6a651327cc19c7fbf216933
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-07 10:23:05 -0800

    Hello world now sucessfully rendering in browser

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 var greeting = React.createElement('h1', {}, 'Hello, World!');
+
+// first we are telling it what to render 'greeting' in this case. next we tell it where to render it.
+
+ReactDOM.render(
+greeting,
+  document.getElementById('react-app-root')
+);
